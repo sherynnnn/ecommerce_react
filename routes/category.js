b13f1fb8b441f72a0256a08a4602f0b6e82531cf
sherynnnn <142745064+sherynnnn@users.noreply.github.com>
2024-12-11 15:21:01 +0800

    Update category.js

--- routes/category.js
+++ routes/category.js
@@ -3,13 +3,16 @@ const express = require('express');
 const Product = require('../models/product');
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  try {
-    const category = await Product.distinct('category');
-    res.status(200).json(category);
-  } catch (err) {
-    res.status(400).send({ error: 'Error fetching category: ' + err.message });
-  }
-});
+
+router.get('/by-category', async (req, res) => {
+    const category = req.query.category;
+    try {
+      const products = await Product.find({ category });
+      res.status(200).json(products);
+    } catch (err) {
+      res.status(400).send({ error: 'Error fetching products by category: ' + err.message });
+    }
+  });
+  
 
 module.exports = router;
