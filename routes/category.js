448067a4868c22c406b73eb7fb0e56f8ce155da3
sherynnnn <142745064+sherynnnn@users.noreply.github.com>
2024-12-11 14:46:15 +0800

    Update category.js

--- routes/category.js
+++ routes/category.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
     const category = await Product.distinct('category');
     res.status(200).json(category);
   } catch (err) {
-    res.status(400).json({ error: 'Error fetching category: ' + err.message });
+    res.status(400).send({ error: 'Error fetching category: ' + err.message });
   }
 });
 
